8815151db04e582af57e54a7603eeb673a0c1275
TanjaJensen <tanja-jensen@outlook.dk>
2017-04-18 21:51:54 +0200

    Ny version af EER

--- SQLScript.sql
+++ SQLScript.sql
@@ -18,13 +18,13 @@ CREATE SCHEMA IF NOT EXISTS `jfog` DEFAULT CHARACTER SET utf8 ;
 USE `jfog` ;
 
 -- -----------------------------------------------------
--- Table `jfog`.`product`
+-- Table `jfog`.`employee`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jfog`.`product` (
-  `id` INT(11) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `measure` DOUBLE NOT NULL,
-  `price` DOUBLE NOT NULL,
+CREATE TABLE IF NOT EXISTS `jfog`.`employee` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `ename` VARCHAR(45) NOT NULL,
+  `ephone` INT(11) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
 ENGINE = InnoDB
@@ -32,19 +32,33 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
+-- Table `jfog`.`product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `jfog`.`product` (
+  `id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `vare_nr.` VARCHAR(45) NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `` VARCHAR(45) NULL DEFAULT 'default',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `jfog`.`order`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `jfog`.`order` (
   `id` INT(11) NOT NULL,
-  `id_product` INT(11) NOT NULL,
+  `product_id` INT(11) NOT NULL,
   `checkout_time` DATETIME(6) NOT NULL,
   `shipping_price` DOUBLE NOT NULL,
-  `delivery_time` DATETIME(6) NOT NULL,
+  `delivery_time` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(),
+  `pid` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `id_product_idx` (`id_product` ASC),
-  CONSTRAINT `id_product`
-    FOREIGN KEY (`id_product`)
+  INDEX `pid_idx` (`pid` ASC),
+  CONSTRAINT `pid`
+    FOREIGN KEY (`pid`)
     REFERENCES `jfog`.`product` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -54,44 +68,31 @@ CREATE TABLE IF NOT EXISTS `jfog`.`order` (
 -- Table `jfog`.`customer`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `jfog`.`customer` (
-  `id` INT(11) NOT NULL,
-  `id_ordre` INT(11) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `fname` VARCHAR(45) NOT NULL,
   `lname` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   `cphone` INT(11) NOT NULL,
   `mail` VARCHAR(45) NOT NULL,
   `authorization` VARCHAR(45) NOT NULL,
+  `eid` INT(11) NULL,
+  `oid` INT(11) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `id_ordre_idx` (`id_ordre` ASC),
-  CONSTRAINT `id_ordre`
-    FOREIGN KEY (`id_ordre`)
+  INDEX `eid_idx` (`eid` ASC),
+  INDEX `oid_idx` (`oid` ASC),
+  CONSTRAINT `eid`
+    FOREIGN KEY (`eid`)
+    REFERENCES `jfog`.`employee` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `oid`
+    FOREIGN KEY (`oid`)
     REFERENCES `jfog`.`order` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `jfog`.`employee`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jfog`.`employee` (
-  `id` INT(11) NOT NULL,
-  `id_ordre` INT(11) NOT NULL,
-  `ename` VARCHAR(45) NOT NULL,
-  `ephone` INT(11) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `id_ordre_idx` (`id_ordre` ASC),
-  CONSTRAINT `id_ordre`
-    FOREIGN KEY (`id_ordre`)
-    REFERENCES `jfog`.`order` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
+AUTO_INCREMENT = 13
 DEFAULT CHARACTER SET = utf8;
 
 
@@ -106,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `jfog`.`product_item` (
   `roof_quantity` INT(11) NOT NULL,
   `roof_pitch` VARCHAR(45) NOT NULL,
   `bracket` INT(11) NOT NULL,
-  INDEX `product_id_idx` (`id` ASC),
+  INDEX `id_idx` (`id` ASC),
   CONSTRAINT `id`
     FOREIGN KEY (`id`)
     REFERENCES `jfog`.`product` (`id`)
